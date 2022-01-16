37a115a9472320d60666a417f96e5fdd62b71701
Harsh Gaikwad <80387918+Voxzee@users.noreply.github.com>
2022-01-16 20:04:03 +0530

    Delete email-extension.php

--- temp coming soon page/email-extension.php
+++
@@ -1,9 +0,0 @@
-<?php
-$to = "voxzee@hexcode.tk";
-$subject = "Impressions";
-$txt = "report issue";
-$headers = "From: sender" . "\r\n" .
-"CC: harsh.vox20@gmail.com";
-
-mail($to,$subject,$txt,$headers);
-?>
