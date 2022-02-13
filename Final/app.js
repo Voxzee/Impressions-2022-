cc249eca905e2f8d62f20b2f110ad6a87a28183a
Harsh Gaikwad <80387918+Voxzee@users.noreply.github.com>
2022-02-14 01:56:39 +0530

    Add files via upload

---
+++ Final/app.js
@@ -0,0 +1,56 @@
+const intro = document.querySelector(".intro");
+const video = intro.querySelector("video");
+const text = intro.querySelector("h1");
+//END SECTION
+const section = document.querySelector("section");
+const end = section.querySelector("h1");
+
+//SCROLLMAGIC
+const controller = new ScrollMagic.Controller();
+
+//Scenes
+let scene = new ScrollMagic.Scene({
+  duration: 2000,
+  triggerElement: intro,
+  triggerHook: 0
+})
+  
+  .setPin(intro)
+  .addTo(controller);
+
+//Text Animation
+const textAnim = TweenMax.fromTo(text, 3, { opacity: 1 }, { opacity: 0 });
+
+let scene2 = new ScrollMagic.Scene({
+  duration: 2000,
+  triggerElement: intro,
+  triggerHook: 0
+})
+  .setTween(textAnim)
+  .addTo(controller);
+
+//Video Animation
+let accelamount = 0.1;
+let scrollpos = 0;
+let delay = 0;
+
+scene.on("update", e => {
+  scrollpos = e.scrollPos / 100;
+});
+
+setInterval(() => {
+  delay += (scrollpos - delay) * accelamount;
+
+  video.currentTime = delay;
+}, 60);
+
+console.log(document.getElementsByClassName('dropdown-content'));
+
+function ShowMeOnHover() {
+    document.getElementsByClassName('dropdown-content')[0].classList.add("dropdown-visible"); 
+} 
+
+
+function HideMeOnHover() {
+  document.getElementsByClassName('dropdown-content')[0].style.display = "hidden";
+}
